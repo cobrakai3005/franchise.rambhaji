f49e924027d1825f92e3ddd57da48795b7a97873
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2026-07-18 14:28:41 +0530

    Fix some production bug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   base: '/franchise/',
+//   plugins: [react(), tailwindcss()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/franchise/',
   plugins: [react(), tailwindcss()],
-})
+});
